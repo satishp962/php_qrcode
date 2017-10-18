fbe19222ece1a682b8feaa3d00689f3dc8ad3829
Satish Pandey <satishp962@gmail.com>
2017-10-18 22:54:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+include 'phpqrcode.php';
+require 'fpdf.php';
+
+function hex2dec($couleur = "#000000"){
+    $R = substr($couleur, 1, 2);
+    $rouge = hexdec($R);
+    $V = substr($couleur, 3, 2);
+    $vert = hexdec($V);
+    $B = substr($couleur, 5, 2);
+    $bleu = hexdec($B);
+    $tbl_couleur = array();
+    $tbl_couleur['R']=$rouge;
+    $tbl_couleur['V']=$vert;
+    $tbl_couleur['B']=$bleu;
+    return $tbl_couleur;
+}
+
+//conversion pixel -> millimeter at 72 dpi
+function px2mm($px){
+    return $px*25.4/72;
+}
+
+function txtentities($html){
+    $trans = get_html_translation_table(HTML_ENTITIES);
+    $trans = array_flip($trans);
+    return strtr($html, $trans);
+}
+
+for ($i=0; $i<=5; $i++)
+{
+    QRcode::png("Satish Pandey", $i.".png");
+}
+
+header('location:pdf2.php');
---
+++ pdf2.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'fpdf.php';
+
+$pdf=new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('helvetica','B', 8);
+$pdf->SetTextColor(0, 0, 0);
+
+$x = 2; $y=2; $row=1; $col=1;
+for ($i=0; $i<=5; $i++)
+{
+    $pdf->Image($i.'.png', $x, $y);
+    $temp_x = $x+15; $temp_y = $y+30;
+    $pdf->SetXY($temp_x, $temp_y);
+    $pdf->Write(0, $i);
+
+    if (($i+1)%8 == 0)
+    {
+        $col += 1;
+
+        if ($col > 6)
+        {
+            $col = 1;
+            $pdf->AddPage();
+            $x = 5; $y = 2;
+        }
+        else
+            $x += 33; $y = 2;
+    }
+    else
+    {
+        $y += 33;
+    }
+}
+
+$pdf->Output();
+exit;
